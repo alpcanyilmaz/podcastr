bd2530e3f7dfe90c1da0509c7e470c433e325958
alpcanyilmaz <alpcanyilmazz@gmail.com>
2024-07-29 14:56:48 +0300

    added leftsidebar and home layout

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                hostname: 'lovely-flamingo-139.convex.cloud',
+                protocol: 'https',
+            }
+        ]
+    }
+};
 
 export default nextConfig;
